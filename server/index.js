b38bf0d4d32e8d35ee30e98518e2486599d48be4
Evan <evanjensen95@gmail.com>
2021-06-24 15:12:08 -0700

    3rd test

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
 
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 })
 
 const port = process.env.PORT || 5040;
