200f2fa0627cc0f5b9442e127411e52dfed662df
Evan <evanjensen95@gmail.com>
2021-06-24 15:30:03 -0700

    10 test

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'))
-app.use('/styles', express.static(path.join(__dirname, '/public')))
+// app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/styles', express.static(path.join(__dirname, 'public/styles.css')))
 
 
 app.get('/', function(req, res) {
