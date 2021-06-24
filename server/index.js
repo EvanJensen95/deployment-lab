152a62263c8732887b9a719c7ec28eeb2e73ad2f
Evan <evanjensen95@gmail.com>
2021-06-24 14:36:52 -0700

    next

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const path = require('path');
 
+app.use(express.static(__dirname + '/public'))
+
+
 app.get('/', function(req, res) {
 
     res.sendFile( path.join(__dirname, '../index.html') );
@@ -13,3 +16,5 @@ const port = process.env.PORT || 5040;
 app.listen(port, function() {
     console.log(`Server is jamming on ${port}`)
 })
+
+
